440b440d62d5a767c7812fb0c8af0420f0f8d072
Filip Pýrek <PyrekFilip@gmail.com>
2021-08-09 13:58:32 +0200

    style: regenerate graphql-codegen

--- packages/graphql-types/src/index.ts
+++ packages/graphql-types/src/index.ts
@@ -2,6 +2,10 @@ export type Maybe<T> = T | null
 export type Exact<T extends { [key: string]: unknown }> = {
 	[K in keyof T]: T[K]
 }
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
+	{ [SubKey in K]?: Maybe<T[SubKey]> }
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
+	{ [SubKey in K]: Maybe<T[SubKey]> }
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
 	ID: string
@@ -9,11 +13,6 @@ export type Scalars = {
 	Boolean: boolean
 	Int: number
 	Float: number
-}
-
-export type Query = {
-	__typename?: 'Query'
-	todos: Array<Todo>
 }
 
 export type Mutation = {
@@ -35,16 +34,21 @@ export type MutationRemoveTodoArgs = {
 	createdTimestamp: Scalars['ID']
 }
 
-export type Todo = {
-	__typename?: 'Todo'
-	text: Scalars['String']
-	createdTimestamp: Scalars['ID']
-	checked: Scalars['Boolean']
+export type Query = {
+	__typename?: 'Query'
+	todos: Array<Todo>
 }
 
 export enum Status {
 	Success = 'SUCCESS',
 	Fail = 'FAIL'
+}
+
+export type Todo = {
+	__typename?: 'Todo'
+	text: Scalars['String']
+	createdTimestamp: Scalars['ID']
+	checked: Scalars['Boolean']
 }
 
 export type FetchTodosQueryVariables = Exact<{ [key: string]: never }>
