92c0cb45fd308cad971f13af38786def8af219b3
Jakub Zemek <jakub.zemek01@currys.co.uk>
2025-09-18 08:20:00 +0200

    plt-387 remove legacy stack

--- packages/timestamp/src/index.ts
+++
@@ -1,4 +0,0 @@
-import moment from 'moment'
-
-export const getTimestamp = (): string =>
-	moment().format('YYYY-MM-DD HH:mm:ss.SSS')
--- prettier.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	semi: false,
-	singleQuote: true,
-	useTabs: true,
-	trailingComma: 'none',
-	plugins: ['prettier-plugin-tailwindcss'],
-	tailwindConfig: './services/frontend/tailwind.config.ts'
-}
