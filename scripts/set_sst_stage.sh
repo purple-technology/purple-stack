ab3c6612ad5b177d4234238c73432e865bb0db86
Jakub Zemek <jzemek@purple-technology.com>
2025-09-19 15:20:59 +0200

    add sst set sst stage script

---
+++ scripts/set_sst_stage.sh
@@ -0,0 +1,8 @@
+# Get current git branch name
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+# Apply regex to replace non-alphanumeric characters (except underscore) with hyphens
+SST_STAGE=$(echo "$BRANCH_NAME" | sed 's/[^a-zA-Z0-9_]/-/g')
+
+# Export the SST_STAGE environment variable
+export SST_STAGE="$SST_STAGE"
