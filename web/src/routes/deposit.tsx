058ee2e5be8cb42b28fe032815e23bbaf01c30a7
Jakub Zemek <jzemek@purple-technology.com>
2025-10-14 19:11:15 +0200

    add homepage libraries links

--- web/src/routes/deposit.tsx
+++
@@ -1,6 +0,0 @@
-import { DepositPage } from '@purple-stack/transaction/web/pages'
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/deposit')({
-	component: DepositPage
-})
--- web/src/routes/withdrawal.tsx
+++
@@ -1,6 +0,0 @@
-import { WithdrawalPage } from '@purple-stack/transaction/web/pages'
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/withdrawal')({
-	component: WithdrawalPage
-})
